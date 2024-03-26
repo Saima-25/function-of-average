ea0d1c6c9b68920a92712930f2d511161a59c234
unknown <maima.alive25@gmail.com>
2024-03-26 21:20:46 +0500

    push this file to github

---
+++ average.js
@@ -0,0 +1,8 @@
+#! /usr/bin/env node
+function average(no1, no2, no3) {
+    let x = no1 + no2 + no3;
+    let avg = x / 3;
+    console.log(avg);
+}
+average(6, 8, 8);
+export {};
---
+++ average.ts
@@ -0,0 +1,8 @@
+#! /usr/bin/env node
+
+function average(no1: number, no2:number, no3:number){
+    let x=no1+no2+no3
+    let avg=x/3
+    console.log(avg)
+}
+average(6,8,8)
